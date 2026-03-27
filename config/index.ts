77b9e6d7172d604ce4d66c73b5bda2fcdeb7620e
lebrancconvas <lebrancconvas@gmail.com>
2026-03-27 22:57:11 +0700

    🔥 chore: remove all files.

--- config/index.ts
+++
@@ -1,7 +0,0 @@
-import path from "path";
-
-const BASE_PATH = path.join(__dirname, "..", "data", "input");
-
-export const CONFIG = {
-  populationStatePath: path.join(BASE_PATH, "population_state_data") 
-};
